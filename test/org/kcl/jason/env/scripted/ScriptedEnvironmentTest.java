9d708855563570dba3ad0da83265199b3441d387
Felipe Meneguzzi <meneguzzi@users.noreply.github.com>
2011-10-01 22:32:57 +0000

    New build.

--- test/org/kcl/jason/env/scripted/ScriptedEnvironmentTest.java
+++ test/org/kcl/jason/env/scripted/ScriptedEnvironmentTest.java
@@ -6,13 +6,11 @@ package org.kcl.jason.env.scripted;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.util.List;
-
 import jason.asSyntax.ASSyntax;
 import jason.asSyntax.Literal;
 import jason.asSyntax.Structure;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
